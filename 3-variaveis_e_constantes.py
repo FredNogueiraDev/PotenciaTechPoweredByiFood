a47a08c9ae57b64e21b1799ed1ea25477c8eaebb
Frederico Nogueira <fredericonogueira01@outlook.com>
2023-06-25 20:01:50 -0300

    3-variaveis e constantes

---
+++ 3-variaveis_e_constantes.py
@@ -0,0 +1,19 @@
+age = 20
+name = "Fred"
+
+print(f"Meu nome é {name} e eu tenho {age} anos.")
+
+age = 28
+name = "João"
+
+print(f"Meu nome é {name} e eu tenho {age} anos.")
+
+#Constantes
+
+STATES = [
+    "SP", 
+    "MG",
+    "RJ"
+]
+
+print(STATES)
