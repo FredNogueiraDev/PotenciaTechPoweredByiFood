180b30a205055368a0e9fca784940d9f225913a8
Frederico Nogueira <fredericonogueira01@outlook.com>
2023-06-27 06:19:14 -0300

    identacao e blocos

---
+++ 8-identacao_e_blocos.py
@@ -0,0 +1,10 @@
+def sacar(valor):
+    saldo = 580
+
+    if saldo >= valor:
+        print("Valor sacado!")
+        print("Retire seu dinheiro.")
+    else:
+        print("Saldo insuficiente.")
+
+sacar (1000)
