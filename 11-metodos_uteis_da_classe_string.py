024744e8ac9570993806c0488c62b9bfa224f086
Frederico Nogueira <fredericonogueira01@outlook.com>
2023-06-28 19:32:27 -0300

    Metodos uteis da classe string

---
+++ 11-metodos_uteis_da_classe_string.py
@@ -0,0 +1,12 @@
+curso = "pYthOn"
+
+print(curso.upper())
+print(curso.lower())
+print(curso.title())
+
+curso = "          Python   "  
+
+print(f"Curso{curso.strip()}Curso")
+print(f"Curso{curso.lstrip()}Curso")
+print(f"Curso{curso.rstrip()}Curso")
+
