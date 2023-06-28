00a6a62918638ff69d5a74cdec791b9a1a13b6cb
Frederico Nogueira <fredericonogueira01@outlook.com>
2023-06-28 19:55:23 -0300

    Interpolação de variáveis

---
+++ 12-interpolacao_de_variaveis.py
@@ -0,0 +1,13 @@
+nome = "Frederico"
+idade = 20
+profissao = "Analista de Dados"
+linguagem = "Python"
+
+print("Olá, me chamo %s, tenho %d anos e atualmente estou estudando para me tornar um %s, para isso, estou me aprimorando no %s." % (nome, idade, profissao, linguagem))
+print(f"Olá, me chamo {nome}, tenho {idade} anos e atualmente estou estudando para me tornar um {profissao}, para isso, estou me aprimorando no {linguagem}.")
+
+
+PI = 3.14159
+
+print(f"Valor de PI: {PI:.2f}")
+print(f"Valor de PI: {PI:100.2f}")
